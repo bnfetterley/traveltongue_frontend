ad531856c944d18f652a8291e331387e9b43db76
Bri Fetterley <bnfetterley@usfca.edu>
2020-07-12 17:14:45 -0400

    updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,54 +1,48 @@
-import React, { Component, useState, useEffect  } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import '../css/Nav.css';
-import 'semantic-ui-css/semantic.min.css'
-import { useSelector, useDispatch } from 'react-redux'
+// import 'semantic-ui-css/semantic.min.css'
+import { useSelector, useDispatch } from 'react-redux';
 import userActions from '../redux/actions';
 
 const NavBar = (props) => {
-    
-        const dispatch = useDispatch();
-        const handleLogout = () => {
-        dispatch(userActions.logoutUser());
-        };
-   
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(userActions.logoutUser());
+  };
 
+  return (
+    <div
+      class="ui secondary  menu"
+      // onClick={this.toggleCollapse}
+    >
+      <div>
+        {' '}
+        <h1 className="logo">TravelTongue</h1>{' '}
+      </div>
+      <a class="item">
+        <Link to="/">Home</Link>
+      </a>
 
-        return (
+      <a onClick={props.onClick} class="item">
+        Become a Foodie
+      </a>
 
-          <div class="ui secondary  menu"   
-          // onClick={this.toggleCollapse}
-          
-          >
-          <div> <h1 className = "logo">TravelTongue</h1>    </div>
-                <a class="item">
-                <Link to="/">Home</Link>
-                </a>
-                
+      <a onClick={props.onLogInClick} class="item">
+        LogIn
+      </a>
 
-                <a onClick={ props.onClick } class="item" >
-                   Become a Foodie 
-                 </a>
+      <a class="item">
+        <Link to="/" onClick={handleLogout}>
+          Logout
+        </Link>
+      </a>
 
-                 <a onClick={ props.onLogInClick } class="item">
-                   LogIn
-                 </a>
-                 
+      <a onClick={props.onClickDish} class="item">
+        Add a Dish
+      </a>
+    </div>
+  );
+};
 
-                 <a class="item">
-                 <Link to="/" onClick={handleLogout}>Logout</Link>
-                 </a>
-
-                 <a onClick={ props.onClickDish} class="item">
-                    Add a Dish
-              
-                 </a>
-
-
-                  </div>
-    
-        )
-    }
-
-
-export default NavBar
+export default NavBar;
--- src/components/SignUpModal.js
+++ src/components/SignUpModal.js
@@ -1,101 +1,113 @@
-import React, { Component, useState } from 'react'
+import React, { Component, useState } from 'react';
 import '../css/SignUpModal.css';
 import { useDispatch } from 'react-redux';
 import userActions from '../redux/actions.js';
 
-
-
-const SignUpModal = props => {
-  
+const SignUpModal = (props) => {
   // Setting up local state using the useState hook
   const [signupForm, setSignupForm] = useState({
     username: '',
-    password: ''
-     })
-  
-  
-    // Controlled form functions
-    const handleChange = e =>
+    password: '',
+  });
+
+  // Controlled form functions
+  const handleChange = (e) =>
     setSignupForm({ ...signupForm, [e.target.name]: e.target.value });
 
+  const divStyle = {
+    display: props.displayModal ? 'block' : 'none',
+  };
+  function closeModal(e) {
+    e.stopPropagation();
+    props.closeModal();
+  }
 
-          const divStyle = { 
-            display: props.displayModal ? 'block' : 'none'
-       };
-       function closeModal(e) {
-          e.stopPropagation()
-          props.closeModal()
-          
-        }
-        
-                // initializing dispatch
-          const dispatch = useDispatch();
+  // initializing dispatch
+  const dispatch = useDispatch();
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     // const { history } = props;
     dispatch(userActions.newUserToDB(signupForm));
     // history.push('/');
 
-    props.closeModal()
-
+    props.closeModal();
   };
 
   // Destructuring keys from our local state to use in the form
   const { username, password } = signupForm;
 
-        // console.log(this.props)
+  // console.log(this.props)
 
-        return (
+  return (
+    //SIGNUPMODAL
+    <div className="container">
+      <div className="modal" onClick={closeModal} style={divStyle}>
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <form onSubmit={(e) => handleSubmit(e)} action="action_page.php">
+            <div className="container">
+              <h1>Join the global foodie fam!</h1>
+              <p>Please fill in this form to create an account.</p>
+              <hr />
 
-           
-          
-          
-          //SIGNUPMODAL 
-          <div className="container">
-          <div className="modal" onClick={ closeModal } style={divStyle}> 
-          <div className="modal-content"   onClick={ e => e.stopPropagation()} >
-          
-          <form onSubmit = {(e) => handleSubmit(e)} action="action_page.php" >
-           <div className="container">
-           <h1>Join the global foodie fam!</h1>
-            <p>Please fill in this form to create an account.</p>
-           <hr/>
+              <label for="username">
+                <b>Username</b>
+              </label>
+              <input
+                name="username"
+                onChange={(e) => handleChange(e)}
+                value={username}
+                type="textarea"
+                placeholder="Enter Username"
+                required
+              />
+              <br></br>
+              <br></br>
+              <label for="psw">
+                <b>Password</b>
+              </label>
+              <input
+                name="password"
+                onChange={(e) => handleChange(e)}
+                value={password}
+                type="password"
+                placeholder="Enter Password"
+                required
+              />
 
-            <label for="username"><b>Username</b></label>
-            <input name = "username" onChange = {(e) => handleChange(e)} value = {username} type="textarea" placeholder="Enter Username" required/>
-             <br></br>
-             <br></br>
-            <label for="psw"><b>Password</b></label>
-            <input name="password" onChange = {(e) => handleChange(e)}  value = {password} type="password" placeholder="Enter Password" required/>
-          
-            <label for="psw-repeat"><b>Repeat Password</b></label>
-           <input type="password" placeholder="Repeat Password" name="psw-repeat" required/>
-           <br></br>
-            <br></br>
-           <div class="clearfix">
-          <br></br>
-          <br></br>
-  <button type="submit" onClick = {props.renderLogin} class="signupbtn">Explore!</button>
-</div>
+              <label for="psw-repeat">
+                <b>Repeat Password</b>
+              </label>
+              <input
+                type="password"
+                placeholder="Repeat Password"
+                name="psw-repeat"
+                required
+              />
+              <br></br>
+              <br></br>
+              <div class="clearfix">
+                <br></br>
+                <br></br>
+                <button
+                  type="submit"
+                  onClick={props.renderLogin}
+                  class="signupbtn"
+                >
+                  Explore!
+                </button>
+              </div>
+            </div>
+          </form>
 
-</div>
-          
-          }
-       
-</form>
+          <span className="close" onClick={closeModal}>
+            {' '}
+            X{' '}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-
-
-              <span className="close" onClick={ closeModal }> X </span>
-       </div>
-     </div>
-     </div>
-     
-        )
-          
-      
-  }
-
-  export default SignUpModal;
+export default SignUpModal;
