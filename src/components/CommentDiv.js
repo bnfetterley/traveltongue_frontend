d25552eca49d902f1836c3f5a2258ad846e829dc
Bri Fetterley <bnfetterley@usfca.edu>
2020-07-30 19:58:52 -0400

    changed some css

--- src/components/CommentDiv.js
+++ src/components/CommentDiv.js
@@ -73,6 +73,5 @@ class CommentDiv extends Component {
     );
   }
 }
-// const style = <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css'/>
 
 export default CommentDiv;
